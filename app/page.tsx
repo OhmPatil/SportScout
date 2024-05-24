6e845382e5c52a1f7c24dfbeedf424687ca3038f
OhmPatil <workpatil10@gmail.com>
2024-05-24 12:47:10 +0530

    - Auto Redirect / to /tournaments

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  redirect('/tournaments')
+
   return (
     <main className="w-[60%]">
       {/* <Navbar/> */}
